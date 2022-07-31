009bcf5451eb44ad067c19ae436b5d729e1dab5e
Gomess <mateus.nicolau@devmagic.com.br>
2022-07-31 17:43:03 -0300

    fix(*): create var in global for test

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,9 +1,6 @@
-import supertest from "supertest"
-import AppExpress from '@src/main/config/appExpress'
-
 describe("Beach forecast functional test", () => {
   it('should return a forecast with just a few times', async () => {
-    const {body, status} = await supertest(await AppExpress.init()).get('/api/company/access');
+    const {body, status} = await global.testRequest.get('/api/company/access');
 
     expect(status).toBe(200)
     expect(body).toEqual({ ok : 1})
---
+++ test/jest.setup.ts
@@ -0,0 +1,7 @@
+import AppExpress from "@src/main/config/appExpress"
+import supertest from "supertest"
+
+beforeAll(async ()=> {
+  const app = await AppExpress.init()
+  global.testRequest = supertest(app)
+})
