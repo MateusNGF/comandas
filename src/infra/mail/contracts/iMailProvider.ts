bd0dbb0bb6b0f8474c6ea9534f02d243278c6377
Goomees <mateus2017nicolau@gmail.com>
2022-12-31 16:41:50 -0300

    feat(mail): create mail provider

---
+++ src/infra/mail/contracts/iMailProvider.ts
@@ -0,0 +1,18 @@
+
+export abstract class iMailProvider {
+    abstract send(body: iMailProvider.ContentEmail) : Promise<boolean>
+} 
+
+export namespace iMailProvider {
+    export type complementAboutEmail = { name?: string; email: string; }
+    export type EmailData = string | complementAboutEmail
+    export type ContentEmail = {
+        to?: EmailData|EmailData[],
+        // from?: EmailData,
+        subject?: string,
+        text?: string,
+        html?: string,
+        templateId?: string,
+    }
+
+}
---
+++ src/infra/mail/sendgrid.mail.ts
@@ -0,0 +1,26 @@
+import { iMailProvider } from "./contracts/iMailProvider";
+import SendGridProvider from '@sendgrid/mail'
+
+
+class SendGrid implements iMailProvider {
+
+  protected readonly API_KEY = process.env.MAIL_API_KEY as string
+  protected readonly Provider: SendGridProvider.MailService
+
+  constructor() {
+    this.Provider = SendGridProvider;
+    this.Provider.setApiKey(this.API_KEY)
+  }
+
+  async send(body : iMailProvider.ContentEmail): Promise<boolean> {
+    
+    const result = await this.Provider.send({
+      ...body,
+      from : process.env.EMAIL_INTERNAL as string
+    } as any)
+
+    return [200, 201].includes(result[0].statusCode)
+  }
+}
+
+export const SendGridMail = new SendGrid()
