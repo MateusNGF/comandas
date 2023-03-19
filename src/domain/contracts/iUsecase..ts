ac4864a64f817b3c8ba0f76909dfd63739056c12
Goomees <mateus.nicolau@devmagic.com.br>
2023-03-19 20:52:48 -0300

    fix(*): rollback handlerError in usecase

--- src/domain/contracts/iUsecase..ts
+++ src/domain/contracts/iUsecase..ts
@@ -1,16 +1,7 @@
 import { iDatabase } from "src/infra/database/contracts";
-import { CUSTOM_ERROR, InternalError } from "../errors";
 
-export abstract class iUsecase {
-  abstract exec(input: any, options ?: iUsecase.Options): Promise<any>;
-
-  handlersErros(error: any): Error {
-    if (error instanceof CUSTOM_ERROR) {
-      throw error
-    } else {
-      throw new InternalError('Internal Error, try later.')
-    }
-  }
+export interface iUsecase {
+  exec(input: any, options ?: iUsecase.Options): Promise<any>;
 }
 
 
