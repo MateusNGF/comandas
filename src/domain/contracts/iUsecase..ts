a45dea53aae7835dd391268ee7b1664a551be698
Goomees <mateus.nicolau@devmagic.com.br>
2023-03-19 19:59:53 -0300

    fix(*): implements handler erro

--- src/domain/contracts/iUsecase..ts
+++ src/domain/contracts/iUsecase..ts
@@ -1,3 +1,13 @@
-export interface iUsecase {
-  exec(input: any, ...args: any[]): Promise<any>;
+import { CUSTOM_ERROR, InternalError } from "../errors";
+
+export abstract class iUsecase {
+  abstract exec(input: any, ...args: any[]): Promise<any>;
+
+  handlersErros(error: any): Error {
+    if (error instanceof CUSTOM_ERROR) {
+      throw error
+    } else {
+      throw new InternalError('Internal Error, try later.')
+    }
+  }
 }
